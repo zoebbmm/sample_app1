d3d9cc5f1ecb5e8d61234afcab1713d946114ca4
zhou <zhou.wei@sv.cmu.edu>
2012-10-11 06:32:06 -0400

    Add SSL in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,10 @@ SampleApp1::Application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
 
+  # Force all access to the app over SSL, use Strict-Transport-Security,
+  # and use secure cookies.
+  config.force_ssl = true
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
