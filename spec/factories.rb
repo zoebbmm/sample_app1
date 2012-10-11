3312dbacfb4f7bad3c17c53b0a9ad655b7920a5b
zhou <zhou.wei@sv.cmu.edu>
2012-10-11 06:30:06 -0400

    Finish user signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+      name     "Wei Zhou"
+	      email    "zoebbmm@gmail.com"
+		      password "foobar"
+			      password_confirmation "foobar"
+				    end
+					end
+
