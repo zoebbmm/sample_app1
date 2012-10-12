b2d606db7e3277e124712474c7a7b5e2c54d9d20
zhou <zhou.wei@sv.cmu.edu>
2012-10-12 18:57:14 -0400

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,14 +46,12 @@ class UsersController < ApplicationController
     @users = User.paginate(page: params[:page])
   end
 
-  private
-
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
-    end
+  def show
+    @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
+
+  private
 
   def correct_user
     @user = User.find(params[:id])
@@ -63,4 +61,6 @@ class UsersController < ApplicationController
   def admin_user
     redirect_to(root_path) unless current_user.admin?
   end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ SampleApp1::Application.routes.draw do
 
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root to: 'static_pages#home'
   match '/signup',  to: 'users#new'
   match '/signin',  to: 'sessions#new'
