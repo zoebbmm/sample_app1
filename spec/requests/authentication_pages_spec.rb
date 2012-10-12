cf114013b333d633f45381a51ae56d8469aa5f59
zhou <zhou.wei@sv.cmu.edu>
2012-10-12 01:19:16 -0400

    Finish sign in

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_selector('title', text: 'Sign in') }
+      it { should have_selector('div.alert.alert-error', text: 'Invalid') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
+
+    it { should have_selector('h1',    text: 'Sign in') }
+    it { should have_selector('title', text: 'Sign in') }
+
+  end
+end
